92cf7a12fb0080ec3ba7fcd16b43d9d44d750982
adamsmietanka <adamsmietanka@outlook.com>
2022-11-04 08:56:53 +0100

    feat: add "authentication"

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { createClient } from "redis";
 
 const config = {
-    name: 'backend',
-    port: 8000,
-    host: '0.0.0.0',
+  name: "backend",
+  port: 8000,
+  host: "0.0.0.0",
 };
-const { createClient } = require('redis');
-const redisClient = createClient({
+
+const funds = {
+  Bob: 1000,
+  Alice: 2000,
+  John: 500,
+};
+const client = createClient({
   socket: {
-      host: 'cache',
-      port: 6379
+    host: "cache",
+    port: 6379,
   },
+});
+
+client.on("error", (err) => {
+  console.log(err);
 });
 
 const app = express();
@@ -20,16 +30,28 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', async (req, res) => {
-    res.status(200).json({value: 500});
+await client.connect();
 
-    redisClient.on("error", (error) => console.error(`Error : ${error}`));
-    await redisClient.connect();
-    await redisClient.set('test', 'tes', function(err, reply) {
+app.get("/", async (req, res) => {
+  await client.set("test", "tes", function (err, reply) {
+    console.log(reply); // OK
+  });
+  const value = await client.get("test");
+  console.log(value);
+  res.status(200).json({ value: 500 });
+});
+
+app.get("/people", async (req, res) => {
+  res.status(200).json(Object.keys(funds));
+});
+
+app.post('/reset/', async (req, res) => {
+    await client.hSet('funds', [...Object.entries(funds).flat()], function(err, reply) {
         console.log(reply); // OK
       });
-    const value = await redisClient.get('test');
+    const value = await client.hGetAll('funds');
     console.log(value)
+    res.status(200).json(value);
 });
 
 app.listen(config.port, config.host);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,63 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [user, setUser] = useState("");
+
   const fetchData = async () => {
-    let response = await axios.get('http://0.0.0.0:8000')
-    console.log(response)
-  }
+    let response = await axios.get("http://0.0.0.0:8000");
+    console.log(response.data);
+  };
+  const fetchPeople = async () => {
+    let response = await axios.get("http://0.0.0.0:8000/people");
+    setPeople(() => response.data);
+    setUser(() => response.data[0]);
+  };
+  const resetData = async () => {
+    let response = await axios.post("http://0.0.0.0:8000/reset/");
+    console.log(response.data);
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    resetData();
+    fetchPeople();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">DockerBank</a>
+      </div>
+      <div className="flex-none">
+        <p>Hi</p>
+        <ul className="menu menu-horizontal p-0">
+          <li tabIndex={0}>
+            <a>
+              {user}
+              <svg
+                className="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul className="p-2 bg-base-100">
+              {people
+                .filter((u) => u !== user)
+                .map((p) => (
+                  <li>
+                    <a onClick={() => setUser(() => p)}>{p}</a>
+                  </li>
+                ))}
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
